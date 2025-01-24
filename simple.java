0b72093afb62fd8c14cd620f6a2746090492e5b1
srushpatange <120079367+srushpatange@users.noreply.github.com>
2025-01-24 22:33:08 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,13 @@
+package DAY1TNSF;
+
+
+public class simple {
+	public static void main(String args[])
+	{
+System.out.println("Hello World");
+		
+		System.out.print("Hello Students");
+		
+	}
+
+}
